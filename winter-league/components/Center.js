78dc1c8033aaf75de835ad7a41dad0b171baf058
Ryan Payne <ryan6payne@gmail.com>
2022-10-09 23:06:46 +0100

    Adding the main bulk of content

--- winter-league/components/Center.js
+++ winter-league/components/Center.js
@@ -1,50 +1,61 @@
-import { shuffle } from "lodash";
-import { useEffect, useState } from "react";
-import Player from "./Player";
-
-const colours = [
-    "from-indigo-500",
-    "from-green-500",
-    "from-red-500",
-    "from-yellow-500",
-]
+import Jeremy from "./Jeremy";
+import Scott from "./Scott";
+import Ryan from "./Ryan";
+import Adrian from "./Adrian";
+import Andy from "./Andy";
+import Mike from "./Mike";
+import Nick from "./Nick";
+import Craig from "./Craig";
 
 function Center(){
-    const [colour, setColour] = useState(null);
-
-    const [ryan, setRyan] = useState(["Ryan Payne", [42 , 41, 40, 40, 36, 33, 32, 36, 36, 37]]);
-    const [scott, setScott] = useState(["Scott Jacquest", [42 , 41, 40, 40, 36, 33, 32, 36, 36, 37]]);
-
-    const [nick, setNick] = useState(["Nick Howell", [42 , 41, 40, 40, 36, 33, 32, 36, 36, 37]]);
-    const [jeremy, setJeremy] = useState(["Jeremy Nevett", [42 , 41, 40, 40, 36, 33, 32, 36, 36, 37]]);
-
-    const [adrian, setAdrian] = useState(["Adrian Hobson", [42 , 41, 40, 40, 36, 33, 32, 36, 36, 37]]);
-    const [mike, setMike] = useState(["Mike Dougan", [42 , 41, 40, 40, 36, 33, 32, 36, 36, 37]]);
-
-    const [andy, setAndy] = useState(["Andrew Feakin", [42 , 41, 40, 40, 36, 33, 32, 36, 36, 37]]);
-    const [craig, setCraig] = useState(["Craig Biggs", [42 , 41, 40, 40, 36, 33, 32, 36, 36, 37]]);
-
-    useEffect(() =>{
-        setColour(shuffle(colours).pop());
-    }, []) 
-
     return(
-        <div className="flex-grow">
-            <section className={`flex items-end space-x-7 bg-gradient-to-b to-slate-400 ${colour} h-40 text-black text-xl padding-8 items-center justify-center`}>
-                <h1 className="">Winter League</h1>
-                
-            </section>
             <div>
-                <Player player={ryan}/>
-                <Player player={scott}/>
-                <Player player={nick}/>
-                <Player player={jeremy}/>
-                <Player player={adrian}/>
-                <Player player={mike}/>
-                <Player player={andy}/>
-                <Player player={craig}/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm">
+                            <Scott/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm">
+                            <Ryan/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm">
+                            <Craig/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm">
+                            <Mike/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm">
+                            <Andy/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm">
+                            <Jeremy/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm">
+                            <Adrian/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm">
+                            <Nick/>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
-        </div>
+
     )
 }
 
--- winter-league/pages/index.js
+++ winter-league/pages/index.js
@@ -1,12 +1,44 @@
 import Center from "../components/Center.js"
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Home() {
   return (
-    <div className="bg-slate-400 h-screen overflow-hidden">
-      <main className="flex">
+    <div className="bg-black h-screen overflow-auto">
+      <main>
+        <div class="snowflakes" aria-hidden="true">
+          <div class="snowflake">
+          ❅
+          </div>
+          <div class="snowflake">
+          ❅
+          </div>
+          <div class="snowflake">
+          ❆
+          </div>
+          <div class="snowflake">
+          ❄
+          </div>
+          <div class="snowflake">
+          ❅
+          </div>
+          <div class="snowflake">
+          ❆
+          </div>
+          <div class="snowflake">
+          ❄
+          </div>
+          <div class="snowflake">
+          ❅
+          </div>
+          <div class="snowflake">
+          ❆
+          </div>
+          <div class="snowflake">
+          ❄
+          </div>
+        </div>
         <Center/>
       </main>
-
     </div>
   )
 }
